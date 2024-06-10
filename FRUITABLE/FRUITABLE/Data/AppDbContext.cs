b621faba0e4b86eeb44e1c8f4f11ebd1f66bcd4c
haci1995 <hajiaghaaa@code.edu.az>
2024-06-10 20:22:21 +0400

    first commit

---
+++ FRUITABLE/FRUITABLE/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FRUITABLE.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
